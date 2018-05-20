eebc7f07d5e0c40acf71fc1a3d1a882ac89ddd9f
26735jmc <13514717+jmcoleman@users.noreply.github.com>
2018-05-20 19:16:20 -0400

    html and pseudo-logic

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+//array with word
+
+//array of guessed letters
+
+//functions:
+// -> showGameStats
+// -> logGameStats
+// -> revealLetter
+
+// objects:
+// -> var game = {... }
+
+document.onkeyup = function(event) {
+    var key = event.key;
+
+    console.log("Key pressed is: " + key);
+    //if key pushed is in the word, then show that letter in the word to guess
+        //func: call method to show letter
+    //else add the letter to the previous guesses letters 
+        //if it hasn't already been added, add it
+
+        //otherwise, do nothing
+
+
+  }
